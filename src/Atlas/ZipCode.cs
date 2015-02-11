b3925cb8cf6d37c3c6e3f2f162d0aebc91226559
Khalid Abuhakmeh <khalid.abuhakmeh@ritterim.com>
2015-02-11 14:44:14 -0500

    update csv files to more complete data
    I attempted to make the data match what is currently in our galaxy
    offering. This will help us replace that web application while also
    adding new functionality to atlax.
    
    #6

--- src/Atlas/ZipCode.cs
+++ src/Atlas/ZipCode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Ritter.Atlas
 {
@@ -11,5 +12,16 @@ namespace Ritter.Atlas
         public float Latitude { get; set; }
 
         public float Longitude { get; set; }
+
+        public float Elevation { get; set; }
+
+        public int TimeZone { get; set; }
+
+        public string PrimaryCity { get; set; }
+
+        public string ScfCode
+        {
+            get { return new string((Id ?? string.Empty).Take(3).ToArray()); }
+        }
     }
 }
